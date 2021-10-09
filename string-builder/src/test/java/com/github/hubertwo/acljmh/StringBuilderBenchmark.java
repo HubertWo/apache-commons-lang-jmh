1b2985f91d5e611805535d7e52f94bf968949d6f
Hubert Wo <2518652+HubertWo@users.noreply.github.com>
2021-10-09 12:21:51 +0200

    Code cleanup

--- string-builder/src/test/java/com/github/hubertwo/acljmh/StringBuilderBenchmark.java
+++ string-builder/src/test/java/com/github/hubertwo/acljmh/StringBuilderBenchmark.java
@@ -4,7 +4,6 @@ import org.apache.commons.lang3.StringUtils;
 import org.openjdk.jmh.annotations.Benchmark;
 
 public class StringBuilderBenchmark {
-
     private static final int[] INT_ARRAY = new int[]{1, 2, 3, 4};
     private static final long[] LONG_ARRAY = new long[]{1L, 2L, 3L, 4L};
     private static final boolean[] BOOLEAN_ARRAY = new boolean[]{false, true, true, false};
@@ -15,18 +14,17 @@ public class StringBuilderBenchmark {
     }
 
     @Benchmark
-    public void stringJoinerPrimitiveInt() {
-        StringUtils.join(INT_ARRAY, DELIMITER);
+    public String stringBuilderPrimitiveInt() {
+        return StringUtils.join(INT_ARRAY, DELIMITER);
     }
 
     @Benchmark
-    public void stringJoinerPrimitiveBoolean() {
-        StringUtils.join(BOOLEAN_ARRAY, DELIMITER);
+    public String stringBuilderPrimitiveBoolean() {
+        return StringUtils.join(BOOLEAN_ARRAY, DELIMITER);
     }
 
     @Benchmark
-    public void stringJoinerPrimitiveLong() {
-        StringUtils.join(LONG_ARRAY, DELIMITER);
+    public String stringBuilderPrimitiveLong() {
+        return StringUtils.join(LONG_ARRAY, DELIMITER);
     }
-
 }
