1bd89f2562c5170070c4b7df3b590933e0a74ef8
Hubert Wo <2518652+HubertWo@users.noreply.github.com>
2021-10-09 12:12:18 +0200

    Simple benchmarks for StringUtils.join

--- string-builder/src/test/java/com/github/hubertwo/acljmh/StringBuilderBenchmark.java
+++ string-builder/src/test/java/com/github/hubertwo/acljmh/StringBuilderBenchmark.java
@@ -1,16 +1,32 @@
 package com.github.hubertwo.acljmh;
 
+import org.apache.commons.lang3.StringUtils;
 import org.openjdk.jmh.annotations.Benchmark;
 
 public class StringBuilderBenchmark {
+
+    private static final int[] INT_ARRAY = new int[]{1, 2, 3, 4};
+    private static final long[] LONG_ARRAY = new long[]{1L, 2L, 3L, 4L};
+    private static final boolean[] BOOLEAN_ARRAY = new boolean[]{false, true, true, false};
+    private static final char DELIMITER = ',';
 
     public static void main(String[] args) throws Exception {
         org.openjdk.jmh.Main.main(args);
     }
 
     @Benchmark
-    public void stringJoinPrimitiveInt() {
+    public void stringJoinerPrimitiveInt() {
+        StringUtils.join(INT_ARRAY, DELIMITER);
+    }
 
+    @Benchmark
+    public void stringJoinerPrimitiveBoolean() {
+        StringUtils.join(BOOLEAN_ARRAY, DELIMITER);
+    }
+
+    @Benchmark
+    public void stringJoinerPrimitiveLong() {
+        StringUtils.join(LONG_ARRAY, DELIMITER);
     }
 
 }
--- string-joiner/src/test/java/com/github/hubertwo/acljmh/StringJoinerBenchmark.java
+++ string-joiner/src/test/java/com/github/hubertwo/acljmh/StringJoinerBenchmark.java
@@ -1,16 +1,31 @@
 package com.github.hubertwo.acljmh;
 
+import org.apache.commons.lang3.StringUtils;
 import org.openjdk.jmh.annotations.Benchmark;
 
 public class StringJoinerBenchmark {
+    private static final int[] INT_ARRAY = new int[]{1, 2, 3, 4};
+    private static final long[] LONG_ARRAY = new long[]{1L, 2L, 3L, 4L};
+    private static final boolean[] BOOLEAN_ARRAY = new boolean[]{false, true, true, false};
+    private static final char DELIMITER = ',';
 
     public static void main(String[] args) throws Exception {
         org.openjdk.jmh.Main.main(args);
     }
 
     @Benchmark
-    public void stringJoinPrimitiveInt() {
+    public String stringJoinerPrimitiveInt() {
+        return StringUtils.join(INT_ARRAY, DELIMITER);
+    }
 
+    @Benchmark
+    public String stringJoinerPrimitiveBoolean() {
+        return StringUtils.join(BOOLEAN_ARRAY, DELIMITER);
+    }
+
+    @Benchmark
+    public String stringJoinerPrimitiveLong() {
+        return StringUtils.join(LONG_ARRAY, DELIMITER);
     }
 
 }
